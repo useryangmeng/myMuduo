67193f95d428254802c74223b41b7f2203e7e60a
mengy <1964571495@qq.com>
2024-10-10 16:42:22 +0800

    提交TcpServer部分代码

--- TcpServer.cc
+++ TcpServer.cc
@@ -1,14 +1,27 @@
 #include "TcpServer.h"
 #include "Acceptor.h"
 #include "EventLoopThreadPool.h"
+#include "TcpConnection.h"
+#include "Logger.h"
 
 #include <strings.h>
+
+EventLoop* CheckLoopNotNull(EventLoop* loop)
+{
+    if(loop == nullptr)
+    {
+        LOG_FATAL("%s:%s:%d mainLoop is nullptr!\n",__FILE__,__FUNCTION__,__LINE__);
+    }
+    return loop;
+}
+
+int num = 0;
 
 TcpServer::TcpServer(EventLoop *loop,
           const InetAddress &listenAddr,
           const std::string &nameArg,
           Option option)
-    : loop_(loop)
+    : loop_(CheckLoopNotNull(loop))
     , ipPort_(listenAddr.toIpPort())
     , name_(nameArg)
     , acceptor_(new Acceptor(loop,listenAddr,option == kReusePort))
@@ -26,18 +39,22 @@ TcpServer::~TcpServer()
     {
         TcpConnectionPtr conn(it.second);
         it.second.reset();
-        conn->getLoop()->runInLoop(std::bind(&TcpConnection::connectDestroyed, conn));
+        //conn->getLoop()->runInLoop(std::bind(&TcpConnection::connectDestroyed, conn));
     }   
 }
 
 void TcpServer::setThreadNum(int threadNum)
 {
-
+    threadPool_->setThreaNum(threadNum);
 }
 
 void TcpServer::start()
 {
-
+    if(started_++ == 0)
+    {
+        threadPool_->start(threadInitCallback_);
+        loop_->runInLoop(std::bind(&Acceptor::listen,acceptor_.get()));
+    }
 }
 
 void TcpServer::newConnection(int sockfd, const InetAddress &peerAddr)
